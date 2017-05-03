74c7cb84a3cd4ca1af6e1b84774d6c51686f3b92
Itamar Abeliovich <iabeliovich@gmail.com>
2017-05-03 15:27:34 +0300

    Added logging to reddit module

--- commands/reddit.py
+++ commands/reddit.py
@@ -1,22 +1,32 @@
 import requests
+import logutil
+
+logger = logutil.get_logger(__name__, 'log.txt')
 
 async def get_post(bot, *args):
     if not len(args):
         return await bot.say('Usage: `!reddit <subreddit-name> <OPTIONAL: name of post>`\n'
                              'Gets the top post from a sub or finds one of the top posts by keyword.')
 
-    res = requests.get('http://reddit.com/r/%s/top/.json')
+    logger.debug('Fetching posts from reddit')
+    res = requests.get('http://reddit.com/r/%s/top/.json' % args[0])
     posts = list(filter(lambda post: not post['over_18'],
                         map(lambda post: post['data'], res.json()['data']['children'])))
 
+    logger.debug('Reddit posts fetched')
     if not len(posts):
+        logger.debug('No posts found')
         return await bot.say('Post not found')
 
     if len(args) >= 2:
+        logger.debug('Finding post by keyword')
         keyword = ' '.join(args[1:])
-        post = next((i for i in posts if keyword in i['title']), None);
+        post = next((i for i in posts if keyword in i['title']), None)
         if post is None:
-            return await bot.say('Post not found');
+            logger.debug('No posts found with the specified keyword: %s' % keyword)
+            return await bot.say('Post not found')
+        logger.debug('Found matching post')
         return await bot.say(post['url'])
 
+    logger.debug('Returning top post from %s' % args[0])
     return await bot.say(posts[0]['url'])
