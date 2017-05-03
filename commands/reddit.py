4fb358ad3a0bcdbd172438e03b08a8269ebde22d
Itamar Abeliovich <iabeliovich@gmail.com>
2017-05-03 16:32:45 +0300

    Search functionality now uses the search api for Reddit

--- commands/reddit.py
+++ commands/reddit.py
@@ -9,7 +9,12 @@ async def get_post(bot, *args):
                              'Gets the top post from a sub or finds one of the top posts by keyword.')
 
     logger.debug('Fetching posts from reddit')
-    res = requests.get('http://reddit.com/r/%s/top/.json' % args[0], headers={'User-Agent': 'The President bot v0.1'})
+
+    url = 'http://reddit.com/r/%s/top/.json' % args[0]
+    if len(args) >= 2:
+        url = 'http://reddit.com/r/%s/search?q=%s&sort=top' % (args[0], args[1])
+
+    res = requests.get(url, headers={'User-Agent': 'The President bot v0.1'})
     json = res.json()
     if json.get('error'):
         logger.error('Error while fetching reddit posts from /r/%s: code %s; message: %s' %
@@ -23,15 +28,15 @@ async def get_post(bot, *args):
         logger.debug('No posts found')
         return await bot.say('Post not found')
 
-    if len(args) >= 2:
-        logger.debug('Finding post by keyword')
-        keyword = ' '.join(args[1:])
-        post = next((i for i in posts if keyword in i['title']), None)
-        if post is None:
-            logger.debug('No posts found with the specified keyword: %s' % keyword)
-            return await bot.say('Post not found')
-        logger.debug('Found matching post')
-        return await bot.say(post['url'])
+    # if len(args) >= 2:
+    #     logger.debug('Finding post by keyword')
+    #     keyword = ' '.join(args[1:])
+    #     post = next((i for i in posts if keyword in i['title']), None)
+    #     if post is None:
+    #         logger.debug('No posts found with the specified keyword: %s' % keyword)
+    #         return await bot.say('Post not found')
+    #     logger.debug('Found matching post')
+    #     return await bot.say(post['url'])
 
     logger.debug('Returning top post from %s' % args[0])
     return await bot.say(posts[0]['url'])
