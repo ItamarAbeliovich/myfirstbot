f2f6a50f13b08eee3134e56a893235758a769c3d
Itamar Abeliovich <iabeliovich@gmail.com>
2017-05-03 16:56:18 +0300

    Added subreddit restriction to reddit search to only search in one sub

--- commands/reddit.py
+++ commands/reddit.py
@@ -12,7 +12,7 @@ async def get_post(bot, *args):
 
     url = 'http://reddit.com/r/%s/top/.json' % args[0]
     if len(args) >= 2:
-        url = 'http://reddit.com/r/%s/search.json?q=%s&sort=top' % (args[0], ' '.join(args[1:]))
+        url = 'http://reddit.com/r/%s/search.json?q=%s&sort=top&restrict_sr=true' % (args[0], ' '.join(args[1:]))
 
     res = requests.get(url, headers={'User-Agent': 'The President bot v0.1'})
     if not res:
